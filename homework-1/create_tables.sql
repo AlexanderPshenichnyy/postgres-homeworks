836e44a2acc300f98e9b8e5c7d4f58998ae224e0
Alexander Pshenichnyy <ter.anti@mail.ru>
2023-10-08 22:55:37 +0300

    Hw-1.

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,31 @@
--- SQL-команды для создания таблиц
+-- drop table orders;
+-- drop table employees;
+-- drop table customers;
+
+
+
+create table employees
+(
+	employee_id int primary key,
+	first_name varchar(128) not null,
+	last_name varchar(128) not null,
+	title varchar(40) not null,
+	birth_date date not null,
+	notes text
+);
+
+create table customers
+(
+	customer_id varchar(5) primary key not null,
+	company_name varchar(128),
+	contact_name varchar(128)
+);
+
+create table orders
+(
+	order_id int primary key,
+	customer_id varchar(5),
+	employee_id int references employees(employee_id) not null,
+	order_date date not null,
+	ship_city varchar(128)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,38 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import os
+import psycopg2
+
+PATH_TO_CSV = 'north_data'
+
+
+def db(table_name, path, csv_file):
+	with psycopg2.connect(
+			host='localhost',
+			database='north',
+			user='postgres',
+			password=os.getenv('psql_pass')
+	) as conn:
+		conn.autocommit = True
+		with conn.cursor() as cur:
+			with open(f'{path}/{csv_file}', 'r', encoding='utf-8') as file:
+				reader = csv.reader(file)
+				next(reader)
+				for i in reader:
+					sql = f'insert into {table_name} values ({", ".join(["%s"] * len(i))})'
+					cur.execute(sql, i)
+					cur.execute(f'select * from {table_name}')
+					new_table = cur.fetchall()
+					for j in new_table:
+						print(j)
+
+	conn.close()
+
+
+# queries = ['north_data/orders_data.csv'
+# 'north_data/employees_data.csv',
+# 'north_data/customers_data.csv']
+
+db('customers', PATH_TO_CSV, 'customers_data.csv')
+db('employees', PATH_TO_CSV, 'employees_data.csv')
+db('orders', PATH_TO_CSV, 'orders_data.csv')
